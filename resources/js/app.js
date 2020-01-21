f1d17d2e8d4aba3334e743fc5ff66bdd61718bf7
Mattia <mattia.dapino@gmail.com>
2020-01-21 15:54:15 +0000

    Resolve "Configurare PWA"

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,35 @@
-require('./bootstrap');
+import routes from "./router";
+import VueRouter from 'vue-router'
+import Vuetify from 'vuetify'
+
+import App from "./App";
+
+window.Vue = require('vue');
+
+Vue.use(VueRouter);
+Vue.use(Vuetify);
+
+
+// require('./bootstrap');
+
+
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ */
+
+// const files = require.context('./', true, /\.vue$/i)
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+
+const router = new VueRouter(routes);
+
+new Vue({
+    render: h => h(App),
+    router,
+}).$mount('#app')
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/{any}', 'Controller@index')->where('any', '.*');
+
+Auth::routes();
+
+//Route::get('/home', 'HomeController@index')->name('home');
