b8595a26955929dea0c12e3f809c704f1d845c3c
Davide Cardo <davide.cardo@manydesigns.com>
2020-02-13 17:51:04 +0100

    stile commenti

--- resources/js/components/Area/CoursesDetails/Comments.vue
+++ resources/js/components/Area/CoursesDetails/Comments.vue
@@ -4,13 +4,17 @@
       <div class="col-12 col-md-12 spacing">
         <div class="align-in-row">
           <font-awesome-icon icon="user" class="icon-size"/>
-          <p class="title-style">{{comment.name}}</p>
+          <p class="name-style">{{comment.name}}</p>
         </div>
       </div>
       <div class="col-12 col-md-12">
-        <div class="align-in-row">
-          <rating-star :rating="comment.rating" :large="false"/>
-          <p class="title-style"><b>{{comment.title}}</b></p>
+        <div class="row" style="line-height: initial">
+          <div class="col-12 col-sm-2">
+            <rating-star :rating="comment.rating" :large="false" />
+          </div>
+          <div class=" col-12 col-sm-6">
+            <p class="title-style"><b>{{comment.title}}</b></p>
+          </div>
         </div>
       </div>
       <div class="col-12 col-md-6">
@@ -32,6 +36,8 @@
 </script>
 
 <style scoped lang="scss">
+
+  $font-title-size : 1.25rem;
 
   .min-height {
     min-height: 140px;
@@ -56,10 +62,18 @@
     margin-bottom: 10px;
   }
 
-  .title-style {
-    font-size: 1.25rem;
+  .name-style{
+    font-size: $font-title-size;
     margin-bottom: 0rem;
     margin-left: 20px;
+  }
+
+  .title-style {
+    font-size: $font-title-size;
+  }
+
+  .half-width{
+    width: 50%;
   }
 
 </style>
