c69177f08d2090693e45197be5bbf8773a947a17
Lorenzo Dainelli <lorenzo.dainelli@manydesigns.com>
2020-02-15 13:11:19 +0100

    Rest per commenti (Da Testare)

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -10,11 +10,13 @@ use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    public function index(Course $course) {
-          return response()->json($course->comments);
+    public function index(Course $course)
+    {
+        return response()->json($course->comments);
     }
 
-    public function put (Request $request, Course $course) {
+    public function put(Request $request, Course $course)
+    {
         $comment = new Comment();
         $comment->title = $request->input('title');
         $comment->text = $request->input('text');
@@ -22,5 +24,20 @@ class CommentController extends Controller
         $owner = User::findOrFail($request->input('user'));
         $comment->user = $owner->id;
         $course->comments()->save($comment);
+    }
+
+    public function delete(Request $request)
+    {
+        $course = Course::findOrFail($request->input('id'));
+        $course->delete();
+    }
+
+    public function update(Request $request)
+    {
+        $comment = Comment::findOrFail($request->input('id'));
+        $comment->title = $request->input('title');
+        $comment->text = $request->input('text');
+        $comment->rating = $request->input('rating');
+        $comment->update();
     }
 }
