2e1d5d4941a974f686ff8488d40a382f51c1c176
Davide Cardo <davide.cardo@manydesigns.com>
2020-02-18 16:46:14 +0100

    Spinner e cardSize

--- resources/js/pages/Area.vue
+++ resources/js/pages/Area.vue
@@ -1,32 +1,45 @@
 <template>
   <div class="container pt-5 pb-4">
-    <div v-if="area">
-        <h5 class="font-weight-bold">{{area.name}}</h5>
+    <div v-if="area || !loading">
+        <h2 class="font-weight-bold">{{area.name}}</h2>
 <!--        <p style="font-size: 13px">{{department.name}}</p>-->
         <div class="row">
-            <course-card v-for="course in area.courses" :key="course.id" :course="course" class="col-6 col-sm-6 col-md-4 col-lg-3"/>
+            <course-card v-for="course in area.courses" :key="course.id" :course="course" class="col-12 col-sm-6 col-md-4 col-lg-3"/>
         </div>
     </div>
+    <spinner v-if="loading" class="large-spinner"/>
   </div>
 </template>
 
 <script>
   import axios from "../axios";
   import CourseCard from "../components/Area/CourseCard";
+  import Spinner from "../components/General/Spinner";
 
   export default {
     name: "Area",
-    components: {CourseCard},
+    components: {Spinner, CourseCard},
     data: () => ({
       area: null,
+      loading:false
     }),
     created() {
+      this.loading = true;
       axios.get(`/api/area/${this.$route.params.area}`).then(response => {
+        this.loading = false;
         this.area = response.data;
       })
     }
   }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+
+  @import "../../sass/variables";
+
+  .large-spinner{
+    color: $primary-blu !important;
+    font-size: 5rem;
+  }
+
 </style>
