85efd76ee2d2a7cb59b8285a1436744254b06503
Lorenzo Dainelli <lorenzo.dainelli@manydesigns.com>
2020-02-18 16:26:37 +0100

    Aggionamento 404

--- resources/js/pages/not_found.vue
+++ resources/js/pages/not_found.vue
@@ -1,6 +1,13 @@
 <template>
-  <div class="title-container">
-    <h1>{{$t('notFound.description')}}</h1>
+  <div class="im-blue-da-ba-dee">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-sm-12 col-md-6 col-lg-6 pt-5" >
+          <img src="/img/reset.svg" alt="login image" class="mb-2" width="100%">
+          <h1 class=" font-weight-bold text-center text-white">{{$t('notFound.description')}}</h1>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,10 +18,5 @@
 </script>
 
 <style scoped>
-  .title-container {
-    width: 100%;
-    text-align: center;
-    padding: 100px;
-  }
 
 </style>
